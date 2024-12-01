f7dfde4e5835f99b4bdde99b71ed7fa1532d339b
DoubleDippinz <oliverwigdor@gmail.com>
2024-12-01 13:21:48 +0000

    Character and Enemy Blueprints

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -7,11 +7,17 @@
 AAuraCharacterBase::AAuraCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = false;
+	
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void AAuraCharacterBase::BeginPlay()
 {
 	Super::BeginPlay();
+
+
 	
 }
 
