3cb9af7938a9052cdd2e1b400de33c91a9b30d78
DoubleDippinz <oliverwigdor@gmail.com>
2024-12-03 15:47:00 +0000

    Aura HUD

---
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,12 @@
+// Copyright DoubleDippinz
+
+
+#include "UI/HUD/AuraHUD.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld(), OverlayWidgetClass);
+	Widget->AddToViewport();
+}
